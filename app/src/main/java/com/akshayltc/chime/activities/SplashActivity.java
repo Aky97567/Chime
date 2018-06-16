5ab32212e4c8954c89e3f1a2710a2822f579bf61
Akshay Choudhry <Aky97567@users.noreply.github.com>
2018-06-16 12:57:24 +0530

    App Init
    Init.
    Added Splash Screen, WIP.

---
+++ app/src/main/java/com/akshayltc/chime/activities/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.akshayltc.chime.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.akshayltc.chime.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
